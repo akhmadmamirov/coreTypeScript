fa4da723ef294cd50f712e648b1bf0b802248562
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-12-15 15:44:59 -0500

    interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,7 +1,4 @@
-interface Vehicle {
-    name: string,
-    year: number, 
-    broken: boolean,
+interface Reportable {
     summary(): string
 }
 
@@ -15,10 +12,18 @@ const oldCivic = {
     }
 }
 
-const printVehicle = (vehicle : Vehicle):void => {
-    console.log(`Name : ${vehicle.name}`)
-    console.log(`Year : ${vehicle.year}`)
-    console.log(`Broken : ${vehicle.broken}`)
+const drink = {
+    color : 'brown',
+    carbonated: true, 
+    sugar: 40,
+    summary(): string {
+        return `My drink has ${this.sugar} grams of sugar.`
+    }
 }
 
-printVehicle(oldCivic)
+const printSummary = (item : Reportable):void => {
+    console.log(`Broken : ${item.summary()}`)
+}
+
+printSummary(oldCivic)
+printSummary(drink)
